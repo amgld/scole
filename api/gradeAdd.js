0e1fe01d6d03d9e63cfa91baa50064cf41822926
amgld <a@goldin.su>
2019-03-27 17:49:02 +0500

    Текущая работа

---
+++ api/gradeAdd.js
@@ -0,0 +1,13 @@
+/**
+ *   ДОБАВЛЕНИЕ ОТМЕТКИ В БАЗУ ДАННЫХ
+ * 
+ *   Copyright © А. М. Гольдин, 2019. a@goldin.su
+ *   Лицензия CC BY-NC-ND Version 4.0
+ *   https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode.ru
+ */
+"use strict";
+
+// Объект с учебными предметами по умолчанию из файла www/js/ini.js
+const ini = require("../www/js/ini.js");
+const subjDef = ini.subjDef;
+
---
+++ api/subjAdd.js
@@ -0,0 +1,28 @@
+/**
+ *   ДОБАВЛЕНИЕ НОМЕРА КЛАССА В КОЛЛЕКЦИЮ CURRIC
+ * 
+ *   Copyright © А. М. Гольдин, 2019. a@goldin.su
+ *   Лицензия CC BY-NC-ND Version 4.0
+ *   https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode.ru
+ */
+"use strict";
+
+// Возвращает "success" либо "none"
+module.exports = async newClassName => {
+   
+   // Проверяем формат пришедшего имени класса
+   const reClassName = /\d{1,2}[A-Я]{1}/;
+   if (!reClassName.test(newClassName)) return "none";
+   
+   // Проверяем, нет ли уже такого класса в списке,
+   // если нет - добавляем, если есть - возвращаем ошибку
+   let res = await dbFind("curric", {type: "class", className: newClassName});
+   if (res.length) return "none";
+   else {
+      let subNames = ["мальч", "дев", "иняз1", "иняз2", "инф1", "инф2"]
+                   . map(x => newClassName + '-' + x);
+      db.curric.insert(
+         {type: "class", className: newClassName, groups: subNames});
+      return "success";
+   }   
+};
