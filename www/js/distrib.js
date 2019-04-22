42c63e7497c039f4f1cf68f171eaf87af2a935a6
amgld <a@goldin.su>
2019-04-22 15:36:04 +0500

    Текущая работа

--- www/js/distrib.js
+++ www/js/distrib.js
@@ -7,7 +7,20 @@
  */
 "use strict";
 
-let distrClList = [], distrSbList = {}, distrThList = [], distrTutList = {};
+let distrClList  = [], distrSbList = {}, distrThList = [],
+    distrTutList = {}, distrObject = {};
+    
+// Формирование таблицы с педагогической нагрузкой данного учителя
+const setThLoadTable = () => {
+   let teach = dqs("#distSelTeach").value,
+       pedLoad = distrObject.teach,
+       inner = "<tr><td>Педагогической нагрузки не найдено.</td></tr>";
+   
+   if (pedLoad) {
+      
+   }
+   dqs("#teachLoad").innerHTML = inner;
+}
 
 // Назначение классного руководителя классу
 const setTutor = (className, tutorLogin) => {
@@ -44,8 +57,15 @@ const createTutorTable = () => {
 // Формирование контента странички
 createSection("distrib", `
    <h3>Распределение педагогической нагрузки</h3>
+   <select id="distSelTeach" onChange="setThLoadTable()"></select>
+   <table id="teachLoad"></table>
+   <select id="distSelSubj"></select>
+   <select id="distSelClass"></select>
+   <button type="button"
+      id="addTeachLoad" onClick="teachLoadAdd()">Добавить</button>
+      
    <h3>Классное руководство</h3>
-   <table><tr><td><img src="static/preloader.gif"></td></tr></table>
+   <table id="tutTbl"><tr><td><img src="static/preloader.gif"></td></tr></table>   
 `);
 
 // Динамически подгружаем списки предметов, классов, классных руководителей
@@ -54,21 +74,54 @@ getContent.distrib = async () => {
    let apiOpt = {method: "POST", cache: "no-cache", body: `{
       "t": "${uCateg}", "l": "${uLogin}", "p": "${uToken}", "f": "subjList"
    }`};
+   
+   // Объект со списком предметов
    let apiResp = await (await fetch("/", apiOpt)).text();
-   let subjListDop = JSON.parse(apiResp);
+   let subjListDop = {};
+   if (apiResp != "none") subjListDop = JSON.parse(apiResp);
    distrSbList = subjSort({...subjDef, ...subjListDop});
    
+   // Массив со списком классов
    apiOpt.body = apiOpt.body.replace("subjList", "classesList");
    apiResp = await (await fetch("/", apiOpt)).text();
-   distrClList = classSort(JSON.parse(apiResp));
+   if (apiResp != "none") distrClList = classSort(JSON.parse(apiResp));
    
+   // Объект со списком классных руководителей
    apiOpt.body = apiOpt.body.replace("classesList", "tutorsList");
    apiResp = await (await fetch("/", apiOpt)).text();
-   distrTutList = JSON.parse(apiResp);
+   if (apiResp != "none") distrTutList = JSON.parse(apiResp);
    
+   // Объект со списком всех учителей
    apiOpt.body = apiOpt.body.replace("tutorsList", "teachList");
    apiResp = await (await fetch("/", apiOpt)).text();
-   distrThList = userSort(JSON.parse(apiResp));
+   if (apiResp != "none") distrThList = userSort(JSON.parse(apiResp));
    
-   dqs("#distrib table").innerHTML = createTutorTable();
+   // Объект с педагогической нагрузкой всех учителей
+   // {"pupkin": {"s110": ["8Б", "10Ж"], "d830": ["8Б"]}, "ivanov": ...}
+   apiOpt.body = apiOpt.body.replace("teachList", "getDistr");
+   apiResp = await (await fetch("/", apiOpt)).text();
+   if (apiResp != "none") distrObject = JSON.parse(apiResp);
+   
+   // Формируем контент таблицы с классными руководителями
+   dqs("#tutTbl").innerHTML = createTutorTable();
+   
+   // Формируем селект выбора учителя
+   let dSelThInner = "<option value=''>&#9947; Выберите учителя</option>";
+   for (let teach of distrThList)
+      dSelThInner += `<option value="${teach.login}">${teach.fio}</option>`;
+   dqs("#distSelTeach").innerHTML = dSelThInner;
+   
+   // Формируем селект выбора предмета
+   let dSelSbInner = '';
+   for (let kod of Object.keys(distrSbList))
+      dSelSbInner += `<option value="${kod}">${distrSbList[kod]}</option>`;
+   dqs("#distSelSubj").innerHTML = dSelSbInner;
+   
+   // Формируем селект выбора класса
+   let dSelClInner = '';
+   for (let cls of distrClList) dSelClInner += `<option>${cls}</option>`;
+   dqs("#distSelClass").innerHTML = dSelClInner;
+   
+   // Очищаем таблицу с педагогической нагрузкой
+   dqs("#teachLoad").innerHTML = "<tr><td>Учитель не выбран.</td></tr>";
 }
