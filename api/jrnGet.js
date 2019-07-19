116ef9c7d351ee6ceabb7f60fa71b304a2e77615
amgld <a@goldin.su>
2019-07-19 12:51:09 +0500

    Текущая работа

--- api/jrnGet.js
+++ api/jrnGet.js
@@ -8,7 +8,7 @@
 // (логин с фронтенда не передается, подписывается скриптом index.js).
 // Возвращает none или объект (ВСЕ темы уроков, независимо от отметок)
 // {
-//    "s410-ivanov": {
+//    "8Б-мальч_s410_ivanov": {
 //       d601: ["Африка", "Учить реки", 8, "нн5"],
 //       ...
 //    }
@@ -17,6 +17,7 @@
 module.exports = async (argArr) => {
    let resp = {};
    try {
+      // Логин учащегося
       let lg = argArr[0].substr(0, 20).trim();
       if (!/^[a-z0-9]+$/.test(lg)) return "none";
       
@@ -28,17 +29,26 @@ module.exports = async (argArr) => {
       if (pup.groups) pupGroups = [pup.Uclass, ...pup.groups];
       
       // Из коллекции topics получаем все темы, дз и веса
-      // для этих классов и подгрупп и пишем это все в массив resp
+      // для этих классов и подгрупп и пишем это все в объект resp
       // (см. описание выше) пока без отметок
       for (let gr of pupGroups) {
          res = await dbFind("topics", {g: gr});
          if (!res.length) return "none";
-         for (let topic of res) {
-            if (!)
+         for (let t of res) {
+            let k = `${gr}_${t.s}_${t.l}`;
+            if (!resp[k]) resp[k] = {};
+            resp[k][t.d] = [t.t, t.h, t.w];
          }
       }      
       
-      resp = pupClasses;
+      // Из коллекции grades получаем все отметки этого ребенка
+      // и подписываем их в объект resp
+      res = await dbFind("grades", {p: lg});
+      for (let otm of res) {
+         let k = `${otm.c}_${otm.s}_${otm.t}`;
+         if (resp[k][otm.d]) resp[k][otm.d].push(otm.g);
+      }
+      
       return JSON.stringify(resp);
    }
    catch(e) {return "none";}
