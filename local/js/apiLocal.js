5ee81f722c4a6249df77b793d15845e8aecf15ad
amgld <a@goldin.su>
2020-03-02 16:08:05 +0500

    Текущая работа

--- local/js/apiLocal.js
+++ local/js/apiLocal.js
@@ -447,12 +447,21 @@ apireq = async (func, args) => {
    
    // ***********************************************************************
    // ПОЛУЧЕНИЕ СПИСКА КЛАССНЫХ РУКОВОДИТЕЛЕЙ
-   // Описание
+   // Возвращает объект {"8А": "pupkin", "8Б": "prujinkin", ...}
 	case "tutorsList":
- 		;
+   try {
+      let res = await dbFind("curric", {type: "class"});
+      let tutList = {};
+      for (let currDoc of res) {
+         let tutLogin = currDoc.tutor || "none";
+         tutList[currDoc.className] = tutLogin;
+      }
+      return JSON.stringify(tutList);
+   }
+   catch(e) {return "{}";}
 	break;
    
 	default: return "none";
-   }
+   } // end of switch
 }
 
