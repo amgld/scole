4604ca6223761b9ea45d4bfb618cc8875fcc3b41
amgld <a@goldin.su>
2020-03-02 16:03:21 +0500

    Текущая работа

--- local/js/apiLocal.js
+++ local/js/apiLocal.js
@@ -414,9 +414,35 @@ apireq = async (func, args) => {
    
    // ***********************************************************************
    // ПОЛУЧЕНИЕ ТЕМ УРОКОВ ОДНОЙ СТРАНИЦЫ
-   // Описание
-	case "topicsGet":
- 		;
+   // В запросе приходят [класс, предмет, учитель]
+   // Возвращается none либо объект (вес и часы - не строки, а числа!):
+   // {
+   //   d601: {
+   //      t: "Африка",
+   //      h: "Учить главу 4",
+   //      w: 4,
+   //      v: 2  // если равно 1, то этого свойства нет
+   //   },
+   //   ...
+   // }
+	case "topicsGet": 
+   try {
+      let gr = argsObj[0].substr(0, 20).trim(),
+          sb = argsObj[1].substr(0,  4).trim(),
+          lg = argsObj[2].substr(0, 20).trim();
+
+      if (!gr || !sb || !lg) return "none";
+      
+      let topics = {};      
+      let res = await dbFind("topics", {g: gr, s: sb, l: lg});      
+      for (let currTopic of res) {
+         topics[currTopic.d] = {t:currTopic.t, h:currTopic.h, w:currTopic.w};
+         if (currTopic.v) (topics[currTopic.d]).v = currTopic.v;
+      }
+      
+      return JSON.stringify(topics);
+   }
+   catch(e) {return "none";}
 	break;
    
    // ***********************************************************************
