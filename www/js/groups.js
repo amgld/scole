3a910d56f0f8fbf8a15323880753e2f23a130fac
amgld <a@goldin.su>
2020-05-11 16:02:14 +0500

    Текущая работа

--- www/js/groups.js
+++ www/js/groups.js
@@ -3,7 +3,115 @@
  *   Copyright © 2020, А.М.Гольдин. Modified BSD License
  */
 "use strict";
+let ingrList = [], ingrTeachList = {};
 
+// Публикация списка групп на страничке из массива ingrArr
+const ingrListPubl = ingrArr => {
+   if (!ingrArr.length) {
+      dqs("#ingrList").innerHTML = "<p>Межклассных групп пока нет</p>";
+      return;
+   }   
+   // Сортируем массив и публикуем с иконками удаления и редактирования
+   ingrArr.sort((a, b) => a[0].localeCompare(b[0], "ru"));
+   let cont = '';
+   for (let cGr of ingrArr) {
+      let
+         divDel   = `<div onclick="ingrDel('${cGr[0]}')"
+            title="Удалить">&#10060;</div>`,
+         divName  = `<div>${cGr[0]}</div>`,
+         divTitle = `<div>${cGr[1]}</div>`,
+         divTeach = `<div>${ingrTeachList[cGr[2]]}</div>`;
+         
+      cont += `<span>${divDel}${divName}${divTitle}${divTeach}</span>`;
+   }
+   dqs("#ingrList").innerHTML = cont;  
+};
+
+// Отправка запроса к API для добавления/редактирования группы
+const ingrEdit = async () => {   
+   let namesArr = ingrList.map(x => x[0]);   
+   
+   let grName  = dqs("#ingrNewNameNum").value + dqs("#ingrNewNameLit").value,
+       grTitle = dqs("#ingrNewTitle").value.trim(),
+       grTeach = dqs("#ingrNewTeach").value;
+   // здесь еще обнуление этих полей!  
+   
+   if (!grTitle) {
+      info(1, "Не указано наименование предмета внеурочной деятельности.");
+      return;
+   }
+   
+   let rN = /^[A-Za-z0-9А-Яа-яЁё(). \-]{2,30}$/;
+   if (!rN.test(grTitle)) {
+      info(1, "Наименование предмета может содержать от 2 до 30 букв русского "
+            + "и латинского алфавитов, дефисов, цифр, скобок, точек и пробелов.");
+      return;
+   }
+   
+   if (namesArr.includes(grName)) {
+      let mess = `Данные группы ${grName} будут перезаписаны. Вы уверены?`;
+      if (!confirm(mess)) return;
+   }
+   
+   let apiResp = await apireq("interGroupEdit", [grName, grTitle, grTeach]);
+   if (apiResp == "none") info(1, "Запрашиваемая операция отклонена.");
+   else {
+      if (namesArr.includes(grName))
+         ingrList[namesArr.indexOf(grName)] = [grName, grTitle, grTeach];
+      else ingrList.push([grName, grTitle, grTeach]);
+      ingrListPubl(ingrList);
+   }
+};
+
+// Удаление группы
+const ingrDel = async (grName) => {
+   if (!confirm("Вы уверены?")) return;
+   let apiResp = await apireq("interGroupDel", grName);
+   if (apiResp == "none") info(1, "Запрашиваемая операция отклонена.");
+   else {
+      ingrList.splice(ingrList.map(x => x[0]).indexOf(grName), 1);
+      ingrListPubl(ingrList);
+   }
+}
+
+// Формирование контента странички
 createSection("groups", `
-   Блок «Межклассные группы учащихся»
+   <h3>Межклассные группы обучающихся</h3>
+   <div id="ingrList"></div><br>
+   <p>При добавлении группы укажите ее условный номер (типа <b>23Б</b>, числа
+      от 20 до 99), наименование предмета внеурочной деятельности (факультатива,
+      кружка, секции и пр., например, <b>Вязание крючком</b> или <b>Олимпиадная
+      химия</b>), для которого создана группа, а также учителя, ведущего занятия
+      в этой группе. Если вы укажете условный номер уже существующей группы, её
+      данные будут перезаписаны.</p>
+   <input type="number" id="ingrNewNameNum" min=20 max=99 placeholder=20>
+   <select id="ingrNewNameLit"></select>
+   <input type="text" id="ingrNewTitle" placeholder="Наименование предмета ВД">
+   <select id="ingrNewTeach"></select>
+   <button type="button" onclick="ingrEdit()">Добавить/Редактировать</button>
 `);
+
+let ingrLiterArr = "АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЫЭЮЯ",
+    ingrSelLiter = '';
+for (let iLit of ingrLiterArr) ingrSelLiter += `<option>${iLit}</option>`;
+dqs("#ingrNewNameLit").innerHTML = ingrSelLiter;
+
+// Получаем массив с логинами и фио учителей; заполняем select выбора учителя.
+// Динамически подгружаем список групп в массив ingrList
+// и публикуем его на страничке (имя метода = имени пункта меню!)
+getContent.groups = async () => {
+   let apiResp = await apireq("teachList");
+   let teachArr = JSON.parse(apiResp);
+   teachArr.sort((a, b) => a.fio.localeCompare(b.fio, "ru"));
+   for (let teach of teachArr)
+      ingrTeachList[teach.login] = teach.fio;
+      
+   let selInner = '';
+   for (let tch of teachArr)
+      selInner += `<option value="${tch.login}">${tch.fio}</option>`;
+   dqs("#ingrNewTeach").innerHTML = selInner;
+   
+   apiResp = await apireq("interGroupList");
+   ingrList = JSON.parse(apiResp);
+   ingrListPubl(ingrList);   
+}
